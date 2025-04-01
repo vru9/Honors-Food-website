7815f6095c14cad1fb5e51ecf08d173fa428a340
rjdp <rajdeepvp273@gmail.com>
2025-04-01 22:27:03 +0530

    Added microsoft Clarity

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,12 +21,23 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <GoogleAnalytics/>
-        
+        <GoogleAnalytics />
+
+        {/* Microsoft Clarity Tracking Script */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/qxbagzg8yj";
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "qxbagzg8yj");
+            `,
+          }}
+        />
       </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
       </body>
     </html>
